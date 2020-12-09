2945e8fdb13a81fed2cfd622bbe27d0eade9e585
gustavogavancho <ggavancho@deepdev.com.mx>
2020-12-08 21:57:40 -0500

    Add project files.

---
+++ MiCurriculum.UI.WPF.Usuario/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MiCurriculum.UI.WPF.Usuario
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
