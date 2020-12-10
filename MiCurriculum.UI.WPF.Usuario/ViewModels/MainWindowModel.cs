56a08bab3757606e24d9d2b13499c216ed00e83c
gustavogavancho <ggavancho@deepdev.com.mx>
2020-12-10 12:20:08 -0500

    Implementación de Custom MessageBox
    Se implementó un custom message box con formatos de "Error", "Información", "Advertencia" y "Confirmación".

--- MiCurriculum.UI.WPF.Usuario/ViewModels/MainWindowModel.cs
+++ MiCurriculum.UI.WPF.Usuario/ViewModels/MainWindowModel.cs
@@ -1,8 +1,10 @@
-﻿using MiCurriculum.UI.WPF.Usuario.Helpers;
+﻿using MiCurriculum.UI.WPF.Usuario.CustomControls;
+using MiCurriculum.UI.WPF.Usuario.Helpers;
 using MiCurriculum.UI.WPF.Usuario.Views;
 using System;
 using System.Linq;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 
 namespace MiCurriculum.UI.WPF.Usuario.ViewModels
@@ -19,7 +21,11 @@ namespace MiCurriculum.UI.WPF.Usuario.ViewModels
 
         private void OnCloseCommand()
         {
-            Application.Current.Windows.OfType<MainWindow>().FirstOrDefault().Close();
+            DialogResult result = CustomMessageBox.Show("¿Esta seguro que desear cerrar la aplicación?", CustomMessageBox.CMessageBoxTitle.Confirmación, CustomMessageBox.CMessageBoxButton.Si, CustomMessageBox.CMessageBoxButton.No);
+            if (result == DialogResult.Yes)
+            {
+                System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault().Close();
+            }
         }
 
     }
